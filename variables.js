a759beb2465d0a2f0104facd2c9bb49ff24805fb
Rajdeep paul <112900557+rajgodgang1@users.noreply.github.com>
2023-09-28 16:51:31 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,172 @@
+yarn_bg = {"Yellow":1,
+"Red":2,
+"Ruby Red":3};
+
+yarn_body = {
+    "Exotic Red":1,
+"Hunter Red":1,
+"Hunter Brown":1,
+"Tamed Brown":1,
+"Hunter Blue":1,
+"Tamed Blue":1,
+"Exotic Blue":1,
+"Hunter Gray":1,
+"Tamed Orange":1,
+"Hunter Orange":1,
+"Exotic Black":1,
+"Tamed Pink":1,
+"Exotic Brown":1,
+"Tamed Gray":1,
+"Hunter Pink":1,
+"Exotic Gray":1,
+"Exotic Orange":1,
+"Exotic Pink":1,
+"Tamed Red":1,
+"Tamed Black":1,
+"Hunter Black":1,
+"Hunter White":1,
+"Exotic White":1,
+"Tamed White":1,
+"Skeleton Hunter":4,
+"Skeleton Tamed":4,
+"Skeleton Exotic":4,
+"Robot Hunter":5,
+"Golden Exotic":5,
+"Ruby Skeleton Exotic":5,
+"Golden Hunter":5,
+"Robot Exotic":5,
+"Golden Tamed":5,
+"Robot Tamed":5,
+"Ruby Skeleton Tamed":5,
+"Ruby Skeleton Hunter":5,
+"Cosmic":15,
+"Enlightened Hunter":15,
+"Glitched":15,
+"Golden":15,
+"Jade":15,
+"Robot":15,
+"Ruby Skeleton":15,
+"Skeleton":15
+};
+
+yarn_eyes = {
+    "Discerning Brown":2,
+    "Smug Brown":2,
+    "Side Eye Brown":2,
+    "Open Brown":2,
+    "Bored Brown":2,
+
+    "Discerning Red":2,
+    "Smug Red":2,
+    "Side Eye Red":2,
+    "Open Red":2,
+    "Bored Red":2,
+
+    "Discerning Gray":2,
+    "Smug Gray":2,
+    "Side Eye Gray":2,
+    "Open Gray":2,
+    "Bored Gray":2,
+
+    "Discerning Blue":3,
+    "Smug Blue":3,
+    "Side Eye Blue":3,
+    "Open Blue":3,
+    "Bored Blue":3,
+
+    "Discerning Green":3,
+    "Smug Green":3,
+    "Side Eye Green":3,
+    "Open Green":3,
+    "Bored Green":3,
+    
+    "Discerning Pink":4,
+    "Smug Pink":4,
+    "Side Eye Pink":4,
+    "Open Pink":4,
+    "Bored Pink":4,
+
+    "Discerning White":4,
+    "Smug White":4,
+    "Side Eye White":4,
+    "Open White":4,
+    "Bored White":4,
+    "Adorable":5,
+    "Pretty":5,
+    "Heart":5,
+    "Moon":5,
+    "Diamond":6,
+    "Fire":6,
+    "Rainbow":6
+}
+
+yarn_eyewear = {
+    "Half Moon Spectacles":1,
+    "Visor":1,
+    "Rose Colored Glasses":1,
+    "Monocle":2,
+    "3d Glasses":3,
+    "Big Tech":3,
+    "Black Rimmed Glasses":2,
+    "Sunglasses":1,
+    "Gazelles":1,
+    "Eyepatch":1
+}
+
+yarn_headwear = {
+    "Karate Band":3,
+"Pirate Hat":3,
+"Cowboy Hat":3,
+"Mohawk Pink":3,
+"Backwards Hat":3,
+"Bow Blue":3,
+"Tiny Crown":3,
+"Mohawk Green":3,
+"Beanie":3,
+"Grail":3,
+"Halo":3,
+"Bow Pink":3,
+"Headband":3,
+
+"Crescent Talisman":5,
+
+"Chromie":3,
+
+"Tiara":4,
+"Moon Hat":4,
+"Gremplin":4,
+"Captain Cap":4,
+"Raincloud":4,
+"Fire":4,
+"Lincoln":4,
+"Flower":4,
+"Headphones":4,
+"Queen Crown":4,
+
+"Skully":5,
+"Rubber Duck":5,
+"Hero Cap":5,
+"Durag":5,
+"Bucket Hat":5,
+"Diamond":5,
+"Witch Hat":5,
+"Aviator Cap":5,
+
+"Forest Ranger":6,
+"Space Helmet":6,
+"Wizard Hat":6,
+"Bandana":6
+}
+yarn_outerwear = {
+    "Bomber Jacket":3,
+    "Jean Jacket":3,
+    "Punk Jacket":3,
+    "Hoodie Down":4,
+    "Gold Chain":4,
+    "Diamond Necklace":5,
+    "Hoodie":8
+}
+
+
+
+
